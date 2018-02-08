94c0dffb92a1c8671f9655bdb6cd8217a6b7bb24
Spencer Perkins <hello@spencerperkins.me>
2018-02-08 12:54:00 -0800

    Suggested changes

--- MapExplorer-macOS/Sources/Controllers/PdfViewController/PdfViewController.swift
+++ MapExplorer-macOS/Sources/Controllers/PdfViewController/PdfViewController.swift
@@ -10,23 +10,19 @@ class PdfViewController: NSViewController {
     @IBOutlet weak var pdfView: PDFView! {
         didSet {
             pdfThumbnailView.pdfView = pdfView
-            pdfView.displayMode = .singlePage
-            pdfView.displayDirection = .horizontal
-            pdfView.autoScales = true
-            pdfView.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
         }
     }
-    @IBOutlet weak var pdfThumbnailView: PDFThumbnailView! {
-        didSet {
-           pdfThumbnailView.backgroundColor = #colorLiteral(red: 0.9961728454, green: 0.9902502894, blue: 1, alpha: 0)
-        }
-    }
+    @IBOutlet weak var pdfThumbnailView: PDFThumbnailView!
 
 
     // MARK: Life-cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        pdfThumbnailView.backgroundColor = #colorLiteral(red: 0.9961728454, green: 0.9902502894, blue: 1, alpha: 0)
+        pdfView.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        pdfView.displayDirection = .horizontal
+        pdfView.autoScales = true
 
         if let url = Bundle.main.url(forResource: "5V Relay", withExtension: "pdf") {
             if let pdfDoc = PDFDocument(url: url) {
