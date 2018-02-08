26ae7cf18364577ef6530c49a835bb951c4afd00
Spencer Perkins <hello@spencerperkins.me>
2018-02-07 16:40:18 -0800

    view controller to display pdfs

---
+++ MapExplorer-macOS/Extensions/PDFView.swift
@@ -0,0 +1,9 @@
+//
+//  PDFView.swift
+//  MapExplorer-macOS
+//
+//  Created by Spencer Perkins on 2018-02-07.
+//  Copyright © 2018 JABT. All rights reserved.
+//
+
+import Foundation
---
+++ MapExplorer-macOS/Sources/Controllers/PdfViewController/PdfViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PdfViewController.swift
+//  MapExplorer-macOS
+//
+//  Created by Spencer Perkins on 2018-02-07.
+//  Copyright © 2018 JABT. All rights reserved.
+//
+
+import Cocoa
+
+class PdfViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
