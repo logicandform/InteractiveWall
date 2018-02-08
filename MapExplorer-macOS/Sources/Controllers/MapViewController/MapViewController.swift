036f9ab867532dac110171edea20dcf11721666f
Tim Davis <davistim@me.com>
2018-02-08 12:22:26 -0800

    Fixed configuration settings.

--- MapExplorer-macOS/Sources/Controllers/MapViewController/MapViewController.swift
+++ MapExplorer-macOS/Sources/Controllers/MapViewController/MapViewController.swift
@@ -57,7 +57,7 @@ class MapViewController: NSViewController, MKMapViewDelegate, NSGestureRecognize
     private var initialized = false
     private var lastMapRect = MKMapRect()
     private var userState = UserActivity.idle
-    private var useCustomTiles = true
+    private var useCustomTiles = false
 
     /// After a longActivityTimeoutPeriod this devices will reset and tell all other devices to follow
     private var isMasterDevice: Bool {
