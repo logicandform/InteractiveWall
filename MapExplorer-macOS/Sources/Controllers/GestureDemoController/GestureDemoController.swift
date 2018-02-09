348463e1c69b41e78c5ab3ad4bf2b9740aabd3d6
Spencer Perkins <hello@spencerperkins.me>
2018-02-09 10:53:19 -0800

    Fixed code signing issue, ccausing binding problem

--- MapExplorer-macOS/Sources/Controllers/GestureDemoController/GestureDemoController.swift
+++ MapExplorer-macOS/Sources/Controllers/GestureDemoController/GestureDemoController.swift
@@ -3,7 +3,7 @@
 import MONode
 
 class GestureDemoController: NSViewController, SocketManagerDelegate, TouchResponder {
-    static let config = NetworkConfiguration(broadcastHost: "192.168.1.255", nodePort: 12222)
+    static let config = NetworkConfiguration(broadcastHost: "192.168.1.255", nodePort: 12211)
 
     let socketManager = SocketManager(networkConfiguration: config)
     var touchHandler: TouchHandler!
