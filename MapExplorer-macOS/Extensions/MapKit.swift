b5257cd2374b37fee221e671601ea159203bc2f9
Tim Davis <davistim@me.com>
2018-02-19 11:19:10 -0800

    Added School model.

--- MapExplorer-macOS/Extensions/MapKit.swift
+++ MapExplorer-macOS/Extensions/MapKit.swift
@@ -48,3 +48,25 @@ extension MKMapSize: Equatable {
         lhs.height /= rhs
     }
 }
+
+extension CLLocationCoordinate2D {
+
+    // Example geolocation string: "Tofino [49.2761659,-126.0563673]"
+    init?(geolocation: String?) {
+        guard let location = geolocation, let openingBracket = location.index(of: "["), let comma = location.index(of: ","), let closingBracket = location.index(of: "]") else {
+            return nil
+        }
+
+        let latitudeStart = location.index(after: openingBracket)
+        let latitudeEnd = location.index(before: comma)
+        let longitudeStart = location.index(after: comma)
+        let longitudeEnd = location.index(before: closingBracket)
+
+        guard let lat = Double(location[latitudeStart...latitudeEnd]), let long = Double(location[longitudeStart...longitudeEnd]) else {
+            return nil
+        }
+
+        self.latitude = lat
+        self.longitude = long
+    }
+}
