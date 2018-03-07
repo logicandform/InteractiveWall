3d4c9389905c66e3e5c190c99fecb2e9858c5b66
Tim Davis <davistim@me.com>
2018-03-07 14:26:06 -0800

    Added network code to MapExplorer.

--- MapExplorer-macOS/Sources/Helpers/CachingNetwork.swift
+++ MapExplorer-macOS/Sources/Helpers/CachingNetwork.swift
@@ -5,22 +5,23 @@ import Alamofire
 import PromiseKit
 import MapKit
 
+
 enum NetworkError: Error {
     case badRequest
     case unauthorized
     case notFound
     case serverError
     case badResponse
+    case serializationError
 }
+
 
 final class CachingNetwork {
 
     private struct Endpoints {
         static let baseURL = "http://localhost:3100"
-        static let eventsURL = baseURL + "/events"
         static let placesURL = baseURL + "/places"
-        static let artifactsURL = baseURL + "/artifacts"
-        static let entitiesURL = baseURL + "/entities"
+        static let schoolsURL = baseURL + "/schools"
     }
 
     private static let credentials: [String: String] = {
@@ -30,7 +31,7 @@ final class CachingNetwork {
     }()
 
 
-    // MARK: API
+    // MARK: Places
 
     static func getPlaces() throws -> Promise<[Place]> {
         let url = Endpoints.placesURL
@@ -40,11 +41,30 @@ final class CachingNetwork {
         }
     }
 
-//    static func getPlace(by id: String) -> Promise<Place> {
-//        let url = Endpoints.placesURL + "/" + id
-//
-//        return Alamofire.request(url, headers: credentials).responseJSON().then { json in
-//            try ResponseHandler.serializePlaces(from: json).first
-//        }
-//    }
+    static func getPlace(by id: String) -> Promise<Place> {
+        let url = Endpoints.placesURL + "/" + id
+
+        return Alamofire.request(url, headers: credentials).responseJSON().then { json in
+            try ResponseHandler.serializePlace(from: json)
+        }
+    }
+
+
+    // MARK: Schools
+
+    static func getSchools() throws -> Promise<[School]> {
+        let url = Endpoints.schoolsURL
+
+        return Alamofire.request(url, headers: credentials).responseJSON().then { json in
+            try ResponseHandler.serializeSchools(from: json)
+        }
+    }
+
+    static func getSchool(by id: String) -> Promise<School> {
+        let url = Endpoints.schoolsURL + "/" + id
+
+        return Alamofire.request(url, headers: credentials).responseJSON().then { json in
+            try ResponseHandler.serializeSchool(from: json)
+        }
+    }
 }
