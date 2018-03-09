aac699252b6617737df611c681d97a94f1773565
Tim Davis <davistim@me.com>
2018-03-09 14:10:32 -0800

    Added RecordViewController with layout.

---
+++ WindowExplorer/Sources/Controllers/RecordViewController/RecordViewController.swift
@@ -0,0 +1,12 @@
+//  Copyright © 2018 JABT. All rights reserved.
+
+import Cocoa
+
+class RecordViewController: NSViewController {
+    static let storyboard = NSStoryboard.Name(rawValue: "Record")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
