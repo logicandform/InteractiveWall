32a1e3cee91a7776ef2c93e79cb30254cc0e8f8f
Jeremy Voldeng <jeremyvoldeng@gmail.com>
2018-03-22 10:50:05 -0700

    Subclassed NSClipView to flip stackView

---
+++ WindowExplorer/Sources/Controllers/RecordViewController/FlipClipView.swift
@@ -0,0 +1,11 @@
+//  Copyright © 2018 JABT. All rights reserved.
+
+import Cocoa
+import AppKit
+
+class FlipClipView: NSClipView {
+
+    override var isFlipped: Bool {
+        return true
+    }
+}
