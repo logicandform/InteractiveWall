742b214524e2d920ce4175b9e86d2f311005e00a
C4Framework <info@c4ios.com>
2018-04-02 13:18:06 -0700

    Adds clamp code

--- WindowExplorer/Extensions/Math.swift
+++ WindowExplorer/Extensions/Math.swift
@@ -7,3 +7,10 @@
 //
 
 import Foundation
+
+public func clamp<T: Comparable>(_ val: T, min: T, max: T) -> T {
+    assert(min < max, "min has to be less than max")
+    if val < min { return min }
+    if val > max { return max }
+    return val
+}
