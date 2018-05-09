2bfb9f55b1f13fdf183c71b464dfd7ba8967b2f7
June Ha <june.ha14@gmail.com>
2018-05-09 12:02:53 -0700

    moved methods to GeocodeHelper class

--- WindowExplorer/Sources/Helpers/GeocodeHelper.swift
+++ WindowExplorer/Sources/Helpers/GeocodeHelper.swift
@@ -1,12 +1,16 @@
 //  Copyright © 2018 JABT. All rights reserved.
 
 import Foundation
+import PromiseKit
+import CoreLocation
 
 final class GeocodeHelper {
 
     static let instance = GeocodeHelper()
 
-    private var provinceSchoolMapping = [String: [School]]() // TODO: change String type to the Province Enum type --> Tim's branch
+    private var provinceSchoolMapping = [String: [School]]()
+    private var geoCodeTimer: Timer?
+    private lazy var geocoder = CLGeocoder()
 
 
     // Use singleton instance
@@ -15,30 +19,98 @@ final class GeocodeHelper {
 
     // MARK: API
 
-//     associateProvinceToSchools
-
     // TODO: change province: String to province: Province Enum
-    func getAllSchools(correspondingTo province: String) -> [School] {
-        // check that the province key exists in our mapping variable
-        // get schools corresponding to the province
-
+    func getAllSchools(correspondingTo province: String) -> [School]? {
         guard let schools = provinceSchoolMapping[province] else {
-            return []
+            return nil
         }
-
         return schools
+    }
+
+    func associateSchoolsToProvinces() {
+        SchoolFactory.getAllSchools { [weak self] (schools) in
+            guard let schools = schools, let schoolsWithLatLong = self?.filter(schools) else {
+                return
+            }
+
+            self?.geoCode(schoolsWithLatLong, then: { (dict) in
+                self?.provinceSchoolMapping = dict
+            })
+        }
     }
 
 
     // MARK: Helpers
 
+    private func filter(_ schools: [School]) -> [School] {
+        return schools.filter {
+            $0.coordinate != nil
+        }
+    }
 
+    /// Reverse geocode school's coordinates to its associated province
+    private func geoCode(_ schools: [School], with results: [String: [School]] = [:], then completionHandler: @escaping ([String: [School]]) -> ()) {
+        guard let school = schools.first,
+            let latitude = school.coordinate?.latitude,
+            let longitude = school.coordinate?.longitude else {
+                completionHandler(results)
+                return
+        }
 
+        let location = CLLocation(latitude: latitude, longitude: longitude)
 
+        geocoder.reverseGeocodeLocation(location) { [weak self] (placemarks, error) in
+            var updatedResults = results
 
+            if let error = error {
+                print(error.localizedDescription)
+                self?.geoCodeTimer?.invalidate()
+                self?.geoCodeTimer = Timer.scheduledTimer(withTimeInterval: 5, repeats: false, block: { [weak self] (timer) in
+                    self?.geoCode(schools, with: results, then: completionHandler)
+                })
 
+            } else if let placemark = placemarks?.first {
 
+                if let province = placemark.administrativeArea {
+                    if let _  = updatedResults[province] {
+                        updatedResults[province]?.append(school)
+                    } else {
+                        updatedResults[province] = [school]
+                    }
+                }
 
+                let remainingSchools = Array(schools[1..<schools.count])
+                self?.geoCode(remainingSchools, with: updatedResults, then: completionHandler)
+            }
+        }
+    }
+
+    private func getProvinces(of schoolBatches: [[School]], then completionHandler: @escaping () -> ()) {
+        guard let schoolBatch = schoolBatches.first else {
+            completionHandler()
+            return
+        }
+
+        geoCode(schoolBatch) { (dict) in
+            // dict
+            let remainingSchoolBatches = Array(schoolBatches[1..<schoolBatches.count])
+            self.getProvinces(of: remainingSchoolBatches, then: completionHandler)
+        }
+    }
 
 }
 
+private extension GeocodeHelper {
+    class SchoolFactory {
+        static func getAllSchools(then completionHandler: @escaping ([School]?) -> Void) {
+            firstly {
+                try CachingNetwork.getSchools()
+            }.then { schools in
+                completionHandler(schools)
+            }.catch { error in
+                print(error.localizedDescription)
+                completionHandler(nil)
+            }
+        }
+    }
+}
