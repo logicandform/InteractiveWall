f03e71f5c087a02c6fa4103419f9531848770933
Tim Davis <davistim@me.com>
2018-05-17 13:42:15 -0700

    Revert "Revert "handle parsing coordinates as per database""
    This reverts commit 6facb14a12b278f7c100837cdc13c9ee80f5fd22.

--- MapExplorer-macOS/Sources/Models/Records/School.swift
+++ MapExplorer-macOS/Sources/Models/Records/School.swift
@@ -13,19 +13,23 @@ class School: Record {
     private struct Keys {
         static let id = "id"
         static let title = "title"
-        static let coordinate = "coordinate"
+        static let latitude = "latitude"
+        static let longitude = "longitude"
     }
 
 
     // MARK: Init
 
     init?(json: JSON) {
-        guard let id = json[Keys.id] as? Int, let title = json[Keys.title] as? String, let coordinateString = json[Keys.coordinate] as? String, let coordinate = CLLocationCoordinate2D(string: coordinateString) else {
+        guard let id = json[Keys.id] as? Int,
+            let title = json[Keys.title] as? String,
+            let latitude = json[Keys.latitude] as? Double,
+            let longitude = json[Keys.longitude] as? Double else {
             return nil
         }
 
         self.id = id
         self.title = title
-        self.coordinate = coordinate
+        self.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
     }
 }
