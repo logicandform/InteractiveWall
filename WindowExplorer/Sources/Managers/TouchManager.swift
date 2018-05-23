0d157b7a6b71a08e8374507584d8b6591ff4b647
Harrison Turley <>
2018-05-22 17:13:55 -0700

    Made small name change

--- WindowExplorer/Sources/Managers/TouchManager.swift
+++ WindowExplorer/Sources/Managers/TouchManager.swift
@@ -104,11 +104,11 @@ final class TouchManager: SocketManagerDelegate {
         let windows = WindowManager.instance.windows.sorted(by: { $0.key.orderedIndex < $1.key.orderedIndex })
 
         if touch.state == .down {
-            if let (window, manager) = windows.first(where: { $0.key.frame.contains(touch.position) }), windowSubviewsContain(window, contains: touch, in: manager.responder) {
+            if let (window, manager) = windows.first(where: { $0.key.frame.contains(touch.position) }), windowSubviews(window, contains: touch, in: manager.responder) {
                 return (window, manager)
             }
         } else {
-            if let (window, manager) = windows.first(where: { $0.value.owns(touch) }), windowSubviewsContain(window, contains: touch, in: manager.responder) {
+            if let (window, manager) = windows.first(where: { $0.value.owns(touch) }), windowSubviews(window, contains: touch, in: manager.responder) {
                 return (window, manager)
             }
         }
@@ -116,7 +116,7 @@ final class TouchManager: SocketManagerDelegate {
         return nil
     }
 
-    private func windowSubviewsContain(_ window: NSWindow, contains touch: Touch, in responder: GestureResponder) -> Bool {
+    private func windowSubviews(_ window: NSWindow, contains touch: Touch, in responder: GestureResponder) -> Bool {
         return responder.subview(contains: touch.position.transformed(to: window.frame))
     }
 
