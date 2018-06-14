a0d875444e57af49c49ea8b3db6d9cf2742d62e8
Harrison Turley <>
2018-06-14 14:51:23 -0700

    Related records height bug fixed

--- WindowExplorer/Sources/Helpers/Style.swift
+++ WindowExplorer/Sources/Helpers/Style.swift
@@ -22,7 +22,7 @@ final class Style {
     let noRelatedItemsColor = NSColor(calibratedRed: 33/255, green: 33/255, blue: 33/255, alpha: 1)
 
     // Windows
-    let recordWindowSize = CGSize(width: 416, height: 640)
+    let recordWindowSize = CGSize(width: 416, height: 650)
     let imageWindowSize = CGSize(width: 640, height: 410)
     let pdfWindowSize = CGSize(width: 600, height: 640)
     let playerWindowSize = CGSize(width: 640, height: 440)
