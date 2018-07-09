64c6d0c7c1220a8afb22562e227db06f33b39154
IRSHDC <>
2018-07-09 09:53:31 -0700

    Removed redis from startup script.

--- startup.sh
+++ startup.sh
@@ -5,8 +5,6 @@ screen -d -m -S tiles-1 node www 4100
 screen -d -m -S tiles-2 node www 4200
 screen -d -m -S tiles-3 node www 4300
 
-screen -d -m -S redis redis-server
-
 cd ~/dev/Caching-Server-UBC
 screen -d -m -S caching-server npm start
 
