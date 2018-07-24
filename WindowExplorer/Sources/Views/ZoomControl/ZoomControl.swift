27599b6b3c6d8391e328da261aff2f32aab92f5c
Harrison Turley <>
2018-07-24 14:21:46 -0700

    Magnification button functionality added

--- WindowExplorer/Sources/Views/ZoomControl/ZoomControl.swift
+++ WindowExplorer/Sources/Views/ZoomControl/ZoomControl.swift
@@ -8,6 +8,8 @@ class ZoomControl: NSView {
 
     @IBOutlet var contentView: NSView!
     @IBOutlet weak var seekBar: NSSlider!
+    @IBOutlet weak var zoomOutButton: NSButton!
+    @IBOutlet weak var zoomInButton: NSButton!
 
     var zoomSliderUpdated: ((CGFloat) -> Void)?
 
@@ -27,6 +29,7 @@ class ZoomControl: NSView {
 
     private struct Constants {
         static let cornerRadius: CGFloat = 5
+        static let magnificationButtonZoom = 0.2
     }
 
 
@@ -57,6 +60,18 @@ class ZoomControl: NSView {
         scrubGesture.gestureUpdated = { [weak self] gesture in
             self?.handleScrubGesture(gesture)
         }
+
+        let zoomInGesture = TapGestureRecognizer()
+        gestureManager.add(zoomInGesture, to: zoomInButton)
+        zoomInGesture.gestureUpdated = { [weak self] gesture in
+            self?.handleZoomInGesture(gesture)
+        }
+
+        let zoomOutGesture = TapGestureRecognizer()
+        gestureManager.add(zoomOutGesture, to: zoomOutButton)
+        zoomOutGesture.gestureUpdated = { [weak self] gesture in
+            self?.handleZoomOutGesture(gesture)
+        }
     }
 
 
@@ -81,6 +96,36 @@ class ZoomControl: NSView {
             let scale = CGFloat(delta + seekBar.minValue)
             updateSeekBarPosition(to: scale)
             zoomSliderUpdated?(scale)
+        default:
+            return
+        }
+    }
+
+    private func handleZoomInGesture(_ gesture: GestureRecognizer) {
+        guard let tap = gesture as? TapGestureRecognizer else {
+            return
+        }
+
+        switch tap.state {
+        case .ended:
+            let scale = seekBar.doubleValue + Constants.magnificationButtonZoom > seekBar.maxValue ? seekBar.maxValue : seekBar.doubleValue + Constants.magnificationButtonZoom
+            updateSeekBarPosition(to: CGFloat(scale))
+            zoomSliderUpdated?(CGFloat(scale))
+        default:
+            return
+        }
+    }
+
+    private func handleZoomOutGesture(_ gesture: GestureRecognizer) {
+        guard let tap = gesture as? TapGestureRecognizer else {
+            return
+        }
+
+        switch tap.state {
+        case .ended:
+            let scale = seekBar.doubleValue - Constants.magnificationButtonZoom < seekBar.minValue ? seekBar.minValue : seekBar.doubleValue - Constants.magnificationButtonZoom
+            updateSeekBarPosition(to: CGFloat(scale))
+            zoomSliderUpdated?(CGFloat(scale))
         default:
             return
         }
