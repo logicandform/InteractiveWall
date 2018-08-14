56c01174219666c45e9dce247063e8cdbe177ffc
IRSHDC <>
2018-08-14 15:26:22 -0700

    Edited startup file to suit supervisor

--- startup.sh
+++ startup.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-cd ~/dev/Tile-Server/bin
-screen -d -m -S tiles-1 node www 4100
-screen -d -m -S tiles-2 node www 4200
-screen -d -m -S tiles-3 node www 4300
-
-cd ~/dev/Caching-Server-UBC
-screen -d -m -S caching-server npm start
-
 # Brings window explorer to the front if its open, otherwise opens it
 SERVICE='WindowExplorer'
 if ps ax | grep -v grep | grep "${SERVICE}" &> /dev/null; then
