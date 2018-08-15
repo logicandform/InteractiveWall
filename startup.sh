39ec1e802ea047fdf9d5016a040a23d43393ca13
IRSHDC <>
2018-08-14 17:06:00 -0700

    Adjusted comments on script

--- startup.sh
+++ startup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-# Brings window explorer to the front if its open, otherwise opens it
 
-#sleep 30s
 while ! ping -W 1 -c 1 8.8.8.8 >&/dev/null; do
   sleep 3
 done
 
+# Brings window explorer to the front if its open, otherwise opens it
 SERVICE='WindowExplorer'
 if ps ax | grep -v grep | grep "${SERVICE}" &> /dev/null; then
     open -a "/Users/irshdc/Library/Developer/Xcode/DerivedData/MapExplorer-cebdevedrroybgdstwjueirgqasq/Build/Products/Debug/WindowExplorer.app"
