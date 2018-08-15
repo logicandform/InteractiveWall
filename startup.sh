2a7fec774dde99c518be1221adf1474b50583285
IRSHDC <>
2018-08-14 17:04:43 -0700

    Added wait for network such that touch data can register

--- startup.sh
+++ startup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Brings window explorer to the front if its open, otherwise opens it
+
+#sleep 30s
+while ! ping -W 1 -c 1 8.8.8.8 >&/dev/null; do
+  sleep 3
+done
+
 SERVICE='WindowExplorer'
 if ps ax | grep -v grep | grep "${SERVICE}" &> /dev/null; then
     open -a "/Users/irshdc/Library/Developer/Xcode/DerivedData/MapExplorer-cebdevedrroybgdstwjueirgqasq/Build/Products/Debug/WindowExplorer.app"
