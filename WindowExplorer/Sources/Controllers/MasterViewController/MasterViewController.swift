f60b26148d90023f383a0a75eb05fc94f3b44b12
Harrison Turley <>
2018-08-22 16:44:27 -0700

    Added basic read/response to cmd

--- WindowExplorer/Sources/Controllers/MasterViewController/MasterViewController.swift
+++ WindowExplorer/Sources/Controllers/MasterViewController/MasterViewController.swift
@@ -157,6 +157,12 @@ class MasterViewController: NSViewController {
         }
     }
 
+    @IBAction func scriptRestartButtonClicked(_ sender: NSButton) {
+        let response = runCommand(cmd: "/bin/sh", args: "-c", "git status")
+        print(response.output)
+        print(response.error)
+    }
+
 
     // MARK: Helpers
 
@@ -238,5 +244,38 @@ class MasterViewController: NSViewController {
                 apply(.closeApplication, status: .disconnected, toScreen: screen)
             }
         }
+    }
+
+    private func runCommand(cmd: String, args: String...) -> (output: [String], error: [String], exitCode: Int32) {
+        var output: [String] = []
+        var error: [String] = []
+
+        let task = Process()
+        task.launchPath = cmd
+        task.arguments = args
+
+        let outpipe = Pipe()
+        task.standardOutput = outpipe
+        let errpipe = Pipe()
+        task.standardError = errpipe
+
+        task.launch()
+
+        let outdata = outpipe.fileHandleForReading.readDataToEndOfFile()
+        if var string = String(data: outdata, encoding: .utf8) {
+            string = string.trimmingCharacters(in: .newlines)
+            output = string.components(separatedBy: "\n")
+        }
+
+        let errdata = errpipe.fileHandleForReading.readDataToEndOfFile()
+        if var string = String(data: errdata, encoding: .utf8) {
+            string = string.trimmingCharacters(in: .newlines)
+            error = string.components(separatedBy: "\n")
+        }
+
+        task.waitUntilExit()
+        let status = task.terminationStatus
+
+        return (output, error, status)
     }
 }
