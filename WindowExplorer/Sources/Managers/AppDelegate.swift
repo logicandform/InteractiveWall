4661bb5a041703594586fe7e07b67e7b8493bf14
Harrison Turley <>
2018-08-28 16:15:59 -0700

    Killing all maps on launch of window explorer

--- WindowExplorer/Sources/Managers/AppDelegate.swift
+++ WindowExplorer/Sources/Managers/AppDelegate.swift
@@ -8,7 +8,7 @@ let style = Style()
 
 
 struct Configuration {
-    static let touchPort: UInt16 = 13003
+    static let touchPort: UInt16 = 13002
     static let serverIP = "10.58.73.211"
     static let broadcastIP = "10.58.73.255"
     static let serverURL = "http://\(serverIP):3000"
@@ -24,14 +24,26 @@ struct Configuration {
 struct Paths {
 //    static let mapExplorer = "/Users/irshdc/Library/Developer/Xcode/DerivedData/MapExplorer-cebdevedrroybgdstwjueirgqasq/Build/Products/Debug/MapExplorer-macOS.app"
 //    static let mapExplorer = "/Users/juneha/Library/Developer/Xcode/DerivedData/MapExplorer-egvtpmlvcohzalbqgbyfrerzbcdi/Build/Products/Debug/MapExplorer-macOS.app"
-    static let mapExplorer = "/Users/Tim/Library/Developer/Xcode/DerivedData/MapExplorer-btnxiobgycwlwddqfdkwxqhmpeum/Build/Products/Debug/MapExplorer-macOS.app"
-//    static let mapExplorer = "/Users/harrisonturley/Library/Developer/Xcode/DerivedData/MapExplorer-advgqestfqggadbethjhtmretrda/Build/Products/Debug/MapExplorer-macOS.app"
+//    static let mapExplorer = "/Users/Tim/Library/Developer/Xcode/DerivedData/MapExplorer-btnxiobgycwlwddqfdkwxqhmpeum/Build/Products/Debug/MapExplorer-macOS.app"
+    static let mapExplorer = "/Users/harrisonturley/Library/Developer/Xcode/DerivedData/MapExplorer-advgqestfqggadbethjhtmretrda/Build/Products/Debug/MapExplorer-macOS.app"
 //    static let mapExplorer = "/Users/spencerperkins/Library/Developer/Xcode/DerivedData/MapExplorer-cfmuxpkdowxydndtaoqzbobzopot/Build/Products/Debug/MapExplorer-macOS.app"
 }
 
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+
+    private struct ConsoleKeys {
+        static let killAllPath = "/usr/bin/killall"
+        static let mapExplorerAppName = "MapExplorer-macOS"
+    }
+
+
+    // MARK: Lifecycle
+
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        run(command: ConsoleKeys.killAllPath, args: ConsoleKeys.mapExplorerAppName)
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         WindowManager.instance.registerForNotifications()
@@ -45,5 +57,27 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationWillTerminate(_ notification: Notification) {
         MasterViewController.instance?.close()
+    }
+
+
+    // MARK: Helpers
+
+    private func run(command: String, args: String...) {
+        guard FileManager.default.fileExists(atPath: command) else {
+            print("Failed: Command \(command) does not exist")
+            return
+        }
+
+        let task = Process()
+        task.launchPath = command
+        task.arguments = args
+
+        let outpipe = Pipe()
+        task.standardOutput = outpipe
+        let errpipe = Pipe()
+        task.standardError = errpipe
+
+        task.launch()
+        task.waitUntilExit()
     }
 }
