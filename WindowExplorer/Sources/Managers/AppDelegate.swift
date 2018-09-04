03e0d0514b11acb5f9021659b2abffa5b66bcfff
Tim Davis <davistim@me.com>
2018-09-04 12:01:43 -0700

    Updated iMac IP address.

--- WindowExplorer/Sources/Managers/AppDelegate.swift
+++ WindowExplorer/Sources/Managers/AppDelegate.swift
@@ -10,7 +10,7 @@ let style = Style()
 
 struct Configuration {
     static let touchPort: UInt16 = 13002
-    static let serverIP = "10.58.73.211"
+    static let serverIP = "10.58.73.183"
     static let broadcastIP = "10.58.73.255"
     static let serverURL = "http://\(serverIP):3000"
     static let appsPerScreen = 2
